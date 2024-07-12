8e38f7555433adaf9b2feeafbc9b21dd52506795
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-07-12 17:38:27 -0400

    Added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ const router = createBrowserRouter ([{
   path: '/',
   element: <App />,
 }]); 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import vue from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "bootstrap/scss/bootstrap";
+        `,
+        quietDeps: true, // Suppress deprecation warnings
+      
+
+      }
+    }
+  }
 })
